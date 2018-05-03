c6e6c955c2297cd8a20cd985fd61fefa121a864d
William_Rubiano <wrubianom1@gmail.com>
2018-05-02 23:33:31 -0500

    first commit

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 // Requires
-
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-
-// inicializar varibales
-
+// Inicializar variables
 var app = express();
 
-// coneccion a la base de datos
 
-mongoose.connection.openUri('mongodb://localhost:27017/hospitaldb', (err, res) => {
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
     if (err) throw err;
 
-    console.log('Base de datos : \x1b[32m%s\x1b[0m', ' online ');
-
-});
-
-// rutas
-
-app.get('/', (req, res, next) => {
-
-    res.status(404).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
 });
 
 
-// escuchar peticiones
+// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
+
+// Escuchar peticiones
 app.listen(3000, () => {
-    console.log('Express server corriendo en el puerto 3000: \x1b[32m%s\x1b[0m', ' online ');
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 });
